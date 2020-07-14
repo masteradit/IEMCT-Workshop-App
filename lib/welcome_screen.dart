5a71a08653d547cc8a1600244b6ef082129c2bff
Master Adit <aditluhadia@gmail.com>
2020-07-14 17:00:32 +0530

    Day 2 - Created initial screens

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
