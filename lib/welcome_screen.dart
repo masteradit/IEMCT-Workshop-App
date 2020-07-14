1895f61353680942d0aa27624013ac69c22fcdf8
Master Adit <aditluhadia@gmail.com>
2020-07-14 17:09:20 +0530

    Updated welcome_screen to return a Scaffold

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -10,6 +10,8 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
   }
 }
